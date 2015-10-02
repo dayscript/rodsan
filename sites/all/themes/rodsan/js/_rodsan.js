2088c93eb721fa553fc946feda2a54018d84f155
jcardenas2015 <jcardenas@dayscript.com>
2015-10-02 14:39:24 -0500

    Many modules updates

--- sites/all/themes/rodsan/js/_rodsan.js
+++ sites/all/themes/rodsan/js/_rodsan.js
@@ -1,1 +1,26 @@
 /* Implement custom javascript here */
+
+(function($) {
+    Drupal.behaviors.rodsan = {
+        attach: function(context, settings){
+
+            // Scroll al hacer click en el enlace de contacto
+            if ( $('.l-footer').length !== 0 ) {
+                $('.contacto-footer').on('click', { elem: '.l-footer' }, scrollToElem );
+            }
+
+
+
+
+
+        }
+    }; //-- end Drupal.behaviors
+
+    function scrollToElem (e) {
+        e.preventDefault();
+        var body = jQuery("html, body"),
+            elHeight = $(e.data.elem).offset().top;
+        body.animate({ scrollTop:elHeight }, '800', 'swing');
+    }
+
+})(jQuery);
