bdaf6d5fd248405f6a1c3a3664125f9ed37f8cb0
jcardenas2015 <jcardenas@dayscript.com>
2015-10-20 17:16:25 -0500

    menu_attributes module

--- sites/all/modules/dayscript/templates/indicadores.tpl.php
+++ sites/all/modules/dayscript/templates/indicadores.tpl.php
@@ -1,66 +1,31 @@
-<<<<<<< HEAD
-
 <div class="indicadores-block">
 	<div class="indicadores-block-content">
-	<?php $data=json_decode($data);
-      
-      foreach ($data as $key => $value) :?>
-	     <?php
-	     $image = ($value->change_percent < 0) ? "down-image":"up-image";
+		<?php $data=json_decode($data);
 
-	     ?>
-	     <div class="content">
-	     <div class="indicadores-name left"><?php print $value->name ?> </div>
-	     <div class="indicadores-value rigth">
-	     	<span class="<?php print $image ?>"></span>
-	     	<span class="indicadores-simbol">$</span>
-	     	<?php print $value->value ?> </div>
-	     </div>
+		foreach ($data as $key => $value) :?>
+			<?php
+			$image = ($value->change_percent < 0) ? "down-image":"up-image";
+
+			?>
+			<div class="content">
+				<div class="indicadores-name left"><?php print $value->name ?> </div>
+				<div class="indicadores-value rigth">
+					<span class="<?php print $image ?>"></span>
+					<span class="indicadores-simbol">$</span>
+					<?php print $value->value ?> </div>
+			</div>
 
 
-	     <!--<?php print $value->date?>
+			<!--<?php print $value->date?>
 	     <?php print $value->short ?>
 	     <?php print $value->value ?>
 	     <?php print $value->unit ?>
 	     <?php print $value->change_percent ?>
 	     <?php print $value->country ?>-->
-    
-
-     <?php endforeach;?>
-
-	 
-	<div>
-=======
-
-<div class="indicadores-block">
-	<div class="indicadores-block-content">
-	<?php $data=json_decode($data);
-      
-      foreach ($data as $key => $value) :?>
-	     <?php
-	     $image = ($value->change_percent < 0) ? "down-image":"up-image";
-
-	     ?>
-	     <div class="content">
-	     <div class="indicadores-name left"><?php print $value->name ?> </div>
-	     <div class="indicadores-value rigth">
-	     	<span class="<?php print $image ?>"></span>
-	     	<span class="indicadores-simbol">$</span>
-	     	<?php print $value->value ?> </div>
-	     </div>
 
 
-	     <!--<?php print $value->date?>
-	     <?php print $value->short ?>
-	     <?php print $value->value ?>
-	     <?php print $value->unit ?>
-	     <?php print $value->change_percent ?>
-	     <?php print $value->country ?>-->
-    
+		<?php endforeach;?>
 
-     <?php endforeach;?>
 
-	 
-	<div>
->>>>>>> 4ba788c41a2239a42459589ea3c2ee9482c6a785
-</div>
+		<div>
+		</div>
